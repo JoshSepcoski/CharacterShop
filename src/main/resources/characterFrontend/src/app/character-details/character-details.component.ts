1fc48e4c08129d5f98ca87ff334d46bc9da43571
JoshSepcoski <joshuasepcoski@gmail.com>
2024-09-30 09:58:43 -0400

    Added New Components

---
+++ src/main/resources/characterFrontend/src/app/character-details/character-details.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character-details',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      character-details works!
+    </p>
+  `,
+  styleUrl: './character-details.component.css'
+})
+export class CharacterDetailsComponent {
+
+}
